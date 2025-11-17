e0a40033de53c662f35d270532c04a7c045e150a
Tushar Jindal <tushar.bzp05@gmail.com>
2025-11-17 10:24:45 -0800

    publishing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+  const base = repoName === `${process.env.GITHUB_ACTOR}.github.io` ? '/' : `/${repoName}/`;
+
+  return {
+    plugins: [react()],
+    base,
+  };
+});
